20bee3ad6ffeef7a496c3e2fb2f6294167bc13a1
cwchanap <cwchanap@connect.ust.hk>
2026-05-09 18:37:07 -0700

    feat: add dungeon item catalog entries

--- scripts/data/items/EquipmentCatalog.cs
+++ scripts/data/items/EquipmentCatalog.cs
@@ -135,4 +135,133 @@ public static class EquipmentCatalog
             Rarity = ItemRarity.Uncommon
         };
     }
+
+    public static EquipmentItem CreateSteelLongsword() => new EquipmentItem
+    {
+        Id = "steel_longsword",
+        DisplayName = "Steel Longsword",
+        Description = "A balanced dungeon-forged blade with a keen edge.",
+        Value = 180,
+        SlotType = EquipmentSlotType.Weapon,
+        AttackBonus = 32,
+        AssetPath = "res://assets/sprites/items/weapons/steel_longsword.png",
+        Rarity = ItemRarity.Uncommon
+    };
+
+    public static EquipmentItem CreateChainMail() => new EquipmentItem
+    {
+        Id = "chain_mail",
+        DisplayName = "Chain Mail",
+        Description = "Interlocked steel rings that turn aside dungeon blades.",
+        Value = 190,
+        SlotType = EquipmentSlotType.Armor,
+        DefenseBonus = 26,
+        HealthBonus = 50,
+        AssetPath = "res://assets/sprites/items/armor/chain_mail.png",
+        Rarity = ItemRarity.Uncommon
+    };
+
+    public static EquipmentItem CreateSteelTowerShield() => new EquipmentItem
+    {
+        Id = "steel_tower_shield",
+        DisplayName = "Steel Tower Shield",
+        Description = "A tall steel shield built for holding narrow corridors.",
+        Value = 170,
+        SlotType = EquipmentSlotType.Shield,
+        DefenseBonus = 18,
+        HealthBonus = 30,
+        AssetPath = "res://assets/sprites/items/shields/steel_tower_shield.png",
+        Rarity = ItemRarity.Uncommon
+    };
+
+    public static EquipmentItem CreateKnightHelm() => new EquipmentItem
+    {
+        Id = "knight_helm",
+        DisplayName = "Knight Helm",
+        Description = "A sturdy helm worn by veteran dungeon guards.",
+        Value = 160,
+        SlotType = EquipmentSlotType.Helmet,
+        DefenseBonus = 8,
+        HealthBonus = 160,
+        AssetPath = "res://assets/sprites/items/helmet/knight_helm.png",
+        Rarity = ItemRarity.Uncommon
+    };
+
+    public static EquipmentItem CreateSwiftBoots() => new EquipmentItem
+    {
+        Id = "swift_boots",
+        DisplayName = "Swift Boots",
+        Description = "Light boots with steel caps and quiet soles.",
+        Value = 165,
+        SlotType = EquipmentSlotType.Shoe,
+        DefenseBonus = 4,
+        SpeedBonus = 8,
+        AssetPath = "res://assets/sprites/items/shoes/swift_boots.png",
+        Rarity = ItemRarity.Uncommon
+    };
+
+    public static EquipmentItem CreateObsidianBlade() => new EquipmentItem
+    {
+        Id = "obsidian_blade",
+        DisplayName = "Obsidian Blade",
+        Description = "A black glass blade that cuts with abyssal sharpness.",
+        Value = 420,
+        SlotType = EquipmentSlotType.Weapon,
+        AttackBonus = 44,
+        SpeedBonus = 4,
+        AssetPath = "res://assets/sprites/items/weapons/obsidian_blade.png",
+        Rarity = ItemRarity.Rare
+    };
+
+    public static EquipmentItem CreateObsidianCarapace() => new EquipmentItem
+    {
+        Id = "obsidian_carapace",
+        DisplayName = "Obsidian Carapace",
+        Description = "Dark armor layered like a dungeon construct's shell.",
+        Value = 480,
+        SlotType = EquipmentSlotType.Armor,
+        DefenseBonus = 36,
+        HealthBonus = 120,
+        AssetPath = "res://assets/sprites/items/armor/obsidian_carapace.png",
+        Rarity = ItemRarity.Rare
+    };
+
+    public static EquipmentItem CreateObsidianGuard() => new EquipmentItem
+    {
+        Id = "obsidian_guard",
+        DisplayName = "Obsidian Guard",
+        Description = "A polished black shield etched with warding lines.",
+        Value = 430,
+        SlotType = EquipmentSlotType.Shield,
+        AttackBonus = 8,
+        DefenseBonus = 26,
+        AssetPath = "res://assets/sprites/items/shields/obsidian_guard.png",
+        Rarity = ItemRarity.Rare
+    };
+
+    public static EquipmentItem CreateObsidianCrown() => new EquipmentItem
+    {
+        Id = "obsidian_crown",
+        DisplayName = "Obsidian Crown",
+        Description = "A crown-like helm humming with dungeon pressure.",
+        Value = 460,
+        SlotType = EquipmentSlotType.Helmet,
+        HealthBonus = 240,
+        SpeedBonus = 8,
+        AssetPath = "res://assets/sprites/items/helmet/obsidian_crown.png",
+        Rarity = ItemRarity.Rare
+    };
+
+    public static EquipmentItem CreateObsidianTreads() => new EquipmentItem
+    {
+        Id = "obsidian_treads",
+        DisplayName = "Obsidian Treads",
+        Description = "Black plated boots that move silently over stone.",
+        Value = 410,
+        SlotType = EquipmentSlotType.Shoe,
+        DefenseBonus = 8,
+        SpeedBonus = 12,
+        AssetPath = "res://assets/sprites/items/shoes/obsidian_treads.png",
+        Rarity = ItemRarity.Rare
+    };
 }
--- scripts/data/items/MonsterPartsCatalog.cs
+++ scripts/data/items/MonsterPartsCatalog.cs
@@ -11,7 +11,8 @@ public static class MonsterPartsCatalog
             DisplayName = "Goblin Ear",
             Description = "A pointed ear from a slain goblin. Collectors pay decent coin.",
             Value = 5,
-            MaxStackOverride = 99
+            MaxStackOverride = 99,
+            AssetPath = "res://assets/sprites/items/monster_parts/goblin_ear.png"
         };
     }
 
@@ -23,7 +24,8 @@ public static class MonsterPartsCatalog
             DisplayName = "Orc Tusk",
             Description = "A heavy yellowed tusk. Alchemists use it in strength tonics.",
             Value = 15,
-            MaxStackOverride = 50
+            MaxStackOverride = 50,
+            AssetPath = "res://assets/sprites/items/monster_parts/orc_tusk.png"
         };
     }
 
@@ -35,7 +37,8 @@ public static class MonsterPartsCatalog
             DisplayName = "Skeleton Bone",
             Description = "An ancient bone rattling with residual dark energy.",
             Value = 12,
-            MaxStackOverride = 99
+            MaxStackOverride = 99,
+            AssetPath = "res://assets/sprites/items/monster_parts/skeleton_bone.png"
         };
     }
 
@@ -47,7 +50,8 @@ public static class MonsterPartsCatalog
             DisplayName = "Spider Silk",
             Description = "Incredibly strong webbing from a cave spider.",
             Value = 20,
-            MaxStackOverride = 50
+            MaxStackOverride = 50,
+            AssetPath = "res://assets/sprites/items/monster_parts/spider_silk.png"
         };
     }
 
@@ -60,7 +64,71 @@ public static class MonsterPartsCatalog
             Description = "A gleaming iridescent scale. Rare and extremely valuable.",
             Value = 200,
             Rarity = ItemRarity.Rare,
-            MaxStackOverride = 10
+            MaxStackOverride = 10,
+            AssetPath = "res://assets/sprites/items/monster_parts/dragon_scale.png"
         };
     }
+
+    public static GeneralItem CreateSentinelCore() => new GeneralItem
+    {
+        Id = "sentinel_core",
+        DisplayName = "Sentinel Core",
+        Description = "A cold stone-and-metal core from a crypt sentinel.",
+        Value = 45,
+        MaxStackOverride = 50,
+        AssetPath = "res://assets/sprites/items/monster_parts/sentinel_core.png"
+    };
+
+    public static GeneralItem CreateHexedCloth() => new GeneralItem
+    {
+        Id = "hexed_cloth",
+        DisplayName = "Hexed Cloth",
+        Description = "A scrap of cursed fabric threaded with dim runes.",
+        Value = 50,
+        MaxStackOverride = 50,
+        AssetPath = "res://assets/sprites/items/monster_parts/hexed_cloth.png"
+    };
+
+    public static GeneralItem CreateSplinteredBone() => new GeneralItem
+    {
+        Id = "splintered_bone",
+        DisplayName = "Splintered Bone",
+        Description = "A sharpened bone fragment from a dungeon archer.",
+        Value = 38,
+        MaxStackOverride = 99,
+        AssetPath = "res://assets/sprites/items/monster_parts/splintered_bone.png"
+    };
+
+    public static GeneralItem CreateRevenantPlate() => new GeneralItem
+    {
+        Id = "revenant_plate",
+        DisplayName = "Revenant Plate",
+        Description = "A battered armor plate still clinging to undead will.",
+        Value = 90,
+        Rarity = ItemRarity.Uncommon,
+        MaxStackOverride = 25,
+        AssetPath = "res://assets/sprites/items/monster_parts/revenant_plate.png"
+    };
+
+    public static GeneralItem CreateGargoyleShard() => new GeneralItem
+    {
+        Id = "gargoyle_shard",
+        DisplayName = "Gargoyle Shard",
+        Description = "A jagged black stone shard from a cursed gargoyle.",
+        Value = 100,
+        Rarity = ItemRarity.Uncommon,
+        MaxStackOverride = 25,
+        AssetPath = "res://assets/sprites/items/monster_parts/gargoyle_shard.png"
+    };
+
+    public static GeneralItem CreateAbyssalSigil() => new GeneralItem
+    {
+        Id = "abyssal_sigil",
+        DisplayName = "Abyssal Sigil",
+        Description = "A small sigil pulsing with abyssal ritual energy.",
+        Value = 140,
+        Rarity = ItemRarity.Rare,
+        MaxStackOverride = 10,
+        AssetPath = "res://assets/sprites/items/monster_parts/abyssal_sigil.png"
+    };
 }

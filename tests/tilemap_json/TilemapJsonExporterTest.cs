f5d3dc481ce8d06f9dc7f2361c39b94b432a8bc8
cwchanap <cwchanap@connect.ust.hk>
2026-05-10 18:48:00 -0700

    test: cover treasure JSON export fallbacks

--- tests/tilemap_json/TilemapJsonExporterTest.cs
+++ tests/tilemap_json/TilemapJsonExporterTest.cs
@@ -73,4 +73,64 @@ public partial class TilemapJsonExporterTest : Node
         AssertThat(model.Entities.TreasureBoxes[0].Items[0].Quantity).IsEqual(1);
         sceneRoot.Free();
     }
+
+    [TestCase]
+    public void ExportScene_ExportsNoTreasureBoxItemsWhenItemIdsAreNull()
+    {
+        var sceneRoot = new Node2D { Name = "TestFloor" };
+        var gridMap = new Node2D { Name = "GridMap" };
+        sceneRoot.AddChild(gridMap);
+        gridMap.Owner = sceneRoot;
+
+        var box = new TreasureBoxSpawn
+        {
+            Name = "TreasureBox_EmptyItems",
+            TreasureBoxId = "TreasureBox_EmptyItems",
+            RewardItemIds = null,
+            RewardItemQuantities = [3]
+        };
+        gridMap.AddChild(box);
+        box.Owner = sceneRoot;
+
+        var exporter = new TilemapJsonExporter();
+
+        var model = exporter.ExportScene(gridMap);
+
+        AssertThat(model).IsNotNull();
+        AssertThat(model!.Entities.TreasureBoxes!.Count).IsEqual(1);
+        AssertThat(model.Entities.TreasureBoxes[0].Items.Count).IsEqual(0);
+        sceneRoot.Free();
+    }
+
+    [TestCase]
+    public void ExportScene_DefaultsTreasureBoxItemQuantityWhenQuantityIndexIsMissing()
+    {
+        var sceneRoot = new Node2D { Name = "TestFloor" };
+        var gridMap = new Node2D { Name = "GridMap" };
+        sceneRoot.AddChild(gridMap);
+        gridMap.Owner = sceneRoot;
+
+        var box = new TreasureBoxSpawn
+        {
+            Name = "TreasureBox_MissingQuantity",
+            TreasureBoxId = "TreasureBox_MissingQuantity",
+            RewardItemIds = ["health_potion", "mana_potion"],
+            RewardItemQuantities = [4]
+        };
+        gridMap.AddChild(box);
+        box.Owner = sceneRoot;
+
+        var exporter = new TilemapJsonExporter();
+
+        var model = exporter.ExportScene(gridMap);
+
+        AssertThat(model).IsNotNull();
+        AssertThat(model!.Entities.TreasureBoxes!.Count).IsEqual(1);
+        AssertThat(model.Entities.TreasureBoxes[0].Items.Count).IsEqual(2);
+        AssertThat(model.Entities.TreasureBoxes[0].Items[0].ItemId).IsEqual("health_potion");
+        AssertThat(model.Entities.TreasureBoxes[0].Items[0].Quantity).IsEqual(4);
+        AssertThat(model.Entities.TreasureBoxes[0].Items[1].ItemId).IsEqual("mana_potion");
+        AssertThat(model.Entities.TreasureBoxes[0].Items[1].Quantity).IsEqual(1);
+        sceneRoot.Free();
+    }
 }

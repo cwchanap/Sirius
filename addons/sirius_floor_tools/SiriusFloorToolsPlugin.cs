cfa90d79859fb1fbc14b890a8b4b7bb2718aa5de
cwchanap <cwchanap@connect.ust.hk>
2026-07-05 10:13:37 -0700

    feat: add Sirius Floor Tools editor plugin skeleton

---
+++ addons/sirius_floor_tools/SiriusFloorToolsPlugin.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+namespace Sirius.FloorTools.Addon;
+
+[Tool]
+public partial class SiriusFloorToolsPlugin : EditorPlugin
+{
+    private SiriusFloorToolsDock _dock;
+
+    public override void _EnterTree()
+    {
+        var dockScene = GD.Load<PackedScene>("res://addons/sirius_floor_tools/SiriusFloorToolsDock.tscn");
+        if (dockScene == null)
+        {
+            GD.PrintErr("[SiriusFloorTools] Dock scene not found");
+            return;
+        }
+        _dock = dockScene.Instantiate<SiriusFloorToolsDock>();
+        AddControlToDock(DockSlot.LeftUl, _dock);
+    }
+
+    public override void _ExitTree()
+    {
+        if (_dock != null)
+        {
+            RemoveControlFromDocks(_dock);
+            _dock.Free();
+            _dock = null;
+        }
+    }
+}

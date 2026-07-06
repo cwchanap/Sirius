358989ae8b67b7d3fe8182481cc21d03df6e442e
cwchanap <cwchanap@connect.ust.hk>
2026-07-05 18:41:42 -0700

    feat: port Floor 3 generation; all floors parity-gated

---
+++ scripts/floor_tools/layouts/Floor3Layout.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace Sirius.FloorTools.Layouts;
+
+public static class Floor3Layout
+{
+    public const int Width = 24;
+    public const int Height = 18;
+
+    public static readonly Vector2I PlayerStart = new(10, 10);
+    public static readonly Vector2I DownStair = new(10, 10);
+}

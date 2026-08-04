d0a942ea658d284892054d0af7a860c83fd0213c
cwchanap <cwchanap@connect.ust.hk>
2026-08-04 01:27:54 -0700

    fix: author showcase focus scope

--- tests/ui/showcase/SiriusUiShowcaseFocusTest.cs
+++ tests/ui/showcase/SiriusUiShowcaseFocusTest.cs
@@ -1,5 +1,6 @@
 using GdUnit4;
 using Godot;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using static GdUnit4.Assertions;
@@ -80,5 +81,66 @@ public partial class SiriusUiShowcaseFocusTest : Node
         AssertThat(visited[1]).IsEqual(last);
         AssertThat(visited[2]).IsEqual(first);
         AssertThat(selected.ButtonPressed).IsTrue();
+    }
+
+    [TestCase]
+    public void AuthoredFocusScope_AllowsOnlyTheExplicitThreeControlLoop()
+    {
+        var sceneSource = FileAccess.GetFileAsString(ScenePath);
+        var first = _showcase.GetNode<Button>("%FocusFirstFixture");
+        var selected = _showcase.GetNode<Button>("%SelectedFocusedFixture");
+        var last = _showcase.GetNode<Button>("%FocusLastFixture");
+        var nativeTabs = _showcase.GetNode<TabContainer>("%NativeTabs");
+
+        AssertThat(first.FocusMode).IsEqual(Control.FocusModeEnum.All);
+        AssertThat(selected.FocusMode).IsEqual(Control.FocusModeEnum.All);
+        AssertThat(last.FocusMode).IsEqual(Control.FocusModeEnum.All);
+        AssertThat(first.FocusNext.ToString()).IsEqual("../SelectedFocusedFixture");
+        AssertThat(first.FocusPrevious.ToString()).IsEqual("../FocusLastFixture");
+        AssertThat(selected.FocusNext.ToString()).IsEqual("../FocusLastFixture");
+        AssertThat(selected.FocusPrevious.ToString()).IsEqual("../FocusFirstFixture");
+        AssertThat(last.FocusNext.ToString()).IsEqual("../FocusFirstFixture");
+        AssertThat(last.FocusPrevious.ToString()).IsEqual("../SelectedFocusedFixture");
+
+        AssertSceneNodeProperty(sceneSource, "FocusFirstFixture", "focus_mode = 2");
+        AssertSceneNodeProperty(sceneSource, "SelectedFocusedFixture", "focus_mode = 2");
+        AssertSceneNodeProperty(sceneSource, "FocusLastFixture", "focus_mode = 2");
+
+        string[] staticPreviewButtonNames =
+        {
+            "PrimaryButtonFixture", "SecondaryButtonFixture", "TertiaryButtonFixture",
+            "WarningButtonFixture", "DestructiveButtonFixture", "LoadingFixture",
+            "IgnitionStandardFixture", "IgnitionCompactFixture", "MotionPlayButton",
+            "StressAction"
+        };
+        foreach (var buttonName in staticPreviewButtonNames)
+        {
+            AssertSceneNodeProperty(sceneSource, buttonName, "focus_mode = 0");
+            AssertThat(_showcase.GetNode<Button>($"%{buttonName}").FocusMode)
+                .IsEqual(Control.FocusModeEnum.None);
+        }
+
+        var tooltipButton = _showcase.FindChild("TooltipButton", true, false) as Button;
+        AssertThat(tooltipButton).IsNotNull();
+        AssertSceneNodeProperty(sceneSource, "TooltipButton", "focus_mode = 0");
+        AssertThat(tooltipButton!.FocusMode).IsEqual(Control.FocusModeEnum.None);
+        AssertSceneNodeProperty(sceneSource, "NativeTabs", "focus_mode = 0");
+        AssertThat(nativeTabs.GetTabBar().FocusMode).IsEqual(Control.FocusModeEnum.None);
+    }
+
+    private static void AssertSceneNodeProperty(
+        string sceneSource,
+        string nodeName,
+        string expectedProperty)
+    {
+        var nodeStart = sceneSource.IndexOf($"[node name=\"{nodeName}\"", StringComparison.Ordinal);
+        AssertThat(nodeStart >= 0).IsTrue();
+        if (nodeStart < 0)
+            return;
+
+        var nextNode = sceneSource.IndexOf("\n[node ", nodeStart + 1, StringComparison.Ordinal);
+        var nodeEnd = nextNode >= 0 ? nextNode : sceneSource.Length;
+        var nodeSource = sceneSource.Substring(nodeStart, nodeEnd - nodeStart);
+        AssertThat(nodeSource.Contains(expectedProperty, StringComparison.Ordinal)).IsTrue();
     }
 }
